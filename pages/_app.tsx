2ff177bec5728ef91118c37afc1e09d4012d4636
Jeesan57 <115409557+Jeesan57@users.noreply.github.com>
2024-11-13 20:45:59 +0600

    clerk auth test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <Component {...pageProps} />
+      </SignedIn>
+    </ClerkProvider>
+  )
 }
