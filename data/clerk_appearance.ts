513a5e00baffb2f2a046008925520aa38f00afaf
marufbinsalim <mdmarufbinsalim@gmail.com>
2024-11-16 05:06:30 +0600

    moved clerk appearance config to it's own file

---
+++ data/clerk_appearance.ts
@@ -0,0 +1,58 @@
+import { dark } from "@clerk/themes";
+
+let appearance = {
+  baseTheme: dark,
+  elements: {
+    modalBackdrop: {
+      position: "fixed",
+      backgroundColor: "rgba(0, 0, 0, 0.8)",
+    },
+    cardBox: {
+      width: "400px",
+      minWidth: "00px",
+      maxWidth: "90vw",
+      height: "70vh",
+      minHeight: "max-content",
+      boxShadow: "none",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    card: {
+      backgroundColor: "#1b1b1b",
+      width: "100%",
+      margin: "0 auto",
+      border: "0.1px solid #b2ebf520",
+    },
+    headerSubtitle: {
+      display: "none",
+    },
+    logoBox: {
+      height: "100px",
+    },
+    logoImage: {
+      borderRadius: "50%",
+    },
+    footer: {
+      display: "none",
+    },
+    modalCloseButton: {
+      "&:hover": {
+        backgroundColor: "#2b2b2b",
+      },
+      "&:focus": {
+        border: "1px solid #dbdbdb50",
+        boxShadow: "none",
+      },
+    },
+    socialButtonsBlockButton: {
+      backgroundColor: "#2b2b2b80",
+      "&:hover": {
+        backgroundColor: "#2b2b2b",
+      },
+      padding: "12px",
+    },
+  },
+};
+
+export { appearance };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,65 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+import { appearance } from "@/data/clerk_appearance";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-        elements: {
-          modalBackdrop: {
-            position: "fixed",
-            backgroundColor: "rgba(0, 0, 0, 0.8)",
-          },
-          cardBox: {
-            width: "400px",
-            minWidth: "00px",
-            maxWidth: "90vw",
-            height: "70vh",
-            minHeight: "max-content",
-            boxShadow: "none",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          },
-          card: {
-            backgroundColor: "#1b1b1b",
-            width: "100%",
-            margin: "0 auto",
-            border: "0.1px solid #b2ebf520",
-          },
-          headerSubtitle: {
-            display: "none",
-          },
-          logoBox: {
-            height: "100px",
-          },
-          logoImage: {
-            borderRadius: "50%",
-          },
-          footer: {
-            display: "none",
-          },
-          modalCloseButton: {
-            "&:hover": {
-              backgroundColor: "#2b2b2b",
-            },
-            "&:focus": {
-              border: "1px solid #dbdbdb50",
-              boxShadow: "none",
-            },
-          },
-          socialButtonsBlockButton: {
-            backgroundColor: "#2b2b2b80",
-            "&:hover": {
-              backgroundColor: "#2b2b2b",
-            },
-            padding: "12px",
-          },
-        },
-      }}
-    >
+    <ClerkProvider appearance={appearance}>
       <Component {...pageProps} />
     </ClerkProvider>
   );
