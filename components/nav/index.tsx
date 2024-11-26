35a37957fc3377e5966ff6f8dbd9257be84d0e6e
marufbinsalim <mdmarufbinsalim@gmail.com>
2024-11-26 23:23:39 +0600

    u

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -164,7 +164,7 @@ function LargeScreenNav({ user }: UserPropType) {
 
           {/* Orders and Cart */}
           <div className="cursor-pointer flex items-center gap-1">
-            <span className=" text-light opacity-[0.5]">
+            <span className=" text-light opacity-[0.5g]">
               Returns <br />& Orders
             </span>
             {returnOrders}
