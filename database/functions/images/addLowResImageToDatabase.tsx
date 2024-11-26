8d6aa285f105d277f6fba9130f179e4fdf88bb2d
marufbinsalim <mdmarufbinsalim@gmail.com>
2024-11-27 02:42:34 +0600

    u

--- database/functions/images/addLowResImageToDatabase.tsx
+++ database/functions/images/addLowResImageToDatabase.tsx
@@ -6,29 +6,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { createCanvas, loadImage } from "canvas";
 
-export async function scaleImageToBase64(arrayBuffer: ArrayBuffer) {
-  try {
-    // Convert ArrayBuffer to a Buffer
-    const buffer = Buffer.from(arrayBuffer);
-
-    // Load the image from the buffer
-    const img = await loadImage(buffer);
-
-    // Create a canvas with the target size
-    const canvas = createCanvas(100, 100);
-    const context = canvas.getContext("2d");
-
-    // Draw the image on the canvas, scaled to 100x100
-    context.drawImage(img, 0, 0, 100, 100);
-
-    // Convert the canvas content to a Base64 string
-    return canvas.toDataURL("image/png");
-  } catch (error) {
-    console.error("Error scaling image:", error);
-    return null;
-  }
-}
-
 // Function to fetch an image from a URL, convert it to base64, and upload to Supabase
 async function addLowResImage(
   id: number,
@@ -37,22 +14,20 @@ async function addLowResImage(
   supabase: SupabaseClient<Database>,
 ) {
   // Step 1: Fetch the image from the URL
-  const response = await fetch(imageUrl);
+
+  // Get the image buffer in JPEG format
+  let response = await fetch(
+    `https://resize.sardo.work/?imageUrl=${imageUrl}&width=${100}&height=${100}&quality=${50}`,
+  );
+
   if (!response.ok) {
     throw new Error(
       `Failed to fetch image from URL: ${imageUrl} with status: ${response.status}`,
     );
   }
-
-  // Get the image buffer in JPEG format
-  let buffer = await response.arrayBuffer();
-  const base64FileData = await scaleImageToBase64(buffer);
-
-  if (!base64FileData)
-    return {
-      data: null,
-      error: " Failed to convert image to base64",
-    };
+  // Step 2: Convert the image to base64
+  const buffer = await response.arrayBuffer();
+  const base64FileData = Buffer.from(buffer).toString("base64");
 
   // Step 3: Decode the base64 string to ArrayBuffer
   const decodedData = decode(base64FileData);
