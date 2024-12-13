c70f27208e10132d4366ebbf3924161ece572c43
Jeesan57 <115409557+Jeesan57@users.noreply.github.com>
2024-12-13 06:23:46 +0600

    viewcartpage ui

--- components/cart/viewCartPage.tsx
+++ components/cart/viewCartPage.tsx
@@ -2,233 +2,263 @@ import React, { useEffect, useState } from "react";
 import { SupabaseWrapper } from "@/database/supabase";
 import { UserResource } from "@clerk/types";
 import {
-  incrementCartItem,
-  decrementCartItem,
-  CartItem,
+    incrementCartItem,
+    decrementCartItem,
+    CartItem,
 } from "@/database/functions/users/cart";
-import { TrashIcon } from "lucide-react";
+import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
 
 interface CartProps {
-  user: UserResource | null | undefined;
+    user: UserResource | null | undefined;
 }
 
 const database = new SupabaseWrapper("CLIENT");
 
 export default function Cart({ user }: CartProps) {
-  const [cartItems, setCartItems] = useState<
-    {
-      product_id: number;
-      variant_id: number;
-      quantity: number;
-      image_id: number;
-      size: string;
-      mock: string;
-      title: string;
-      color: string;
-    }[]
-  >([]);
-  const [loading, setLoading] = useState<boolean>(false);
+    const [cartItems, setCartItems] = useState<
+        {
+            product_id: number;
+            variant_id: number;
+            quantity: number;
+            image_id: number;
+            size: string;
+            mock: string;
+            title: string;
+            color: string;
+            price: number;
+        }[]
+    >([]);
+    const [loading, setLoading] = useState<boolean>(false);
 
-  async function fetchCartData() {
-    setLoading(true);
-    try {
-      // Fetch basic cart items
-      const { result: cartItems, error: cartError } =
-        await database.getCartItems(user!.id);
+    async function fetchCartData() {
+        setLoading(true);
+        try {
+            const { result: cartItems, error: cartError } =
+                await database.getCartItems(user!.id);
 
-      if (cartError) {
-        console.error("Error fetching cart items:", cartError);
-        setLoading(false);
-        return;
-      }
+            if (cartError) {
+                console.error("Error fetching cart items:", cartError);
+                setLoading(false);
+                return;
+            }
 
-      if (cartItems) {
-        // Fetch additional details for each cart item
-        const detailedItems = await Promise.all(
-          cartItems.map(async (item: CartItem) => {
-            try {
-              const { result, error } =
-                await database.getProductVariantMockAfterJoin(
-                  item.product_id,
-                  item.variant_id,
-                  item.image_id,
+            if (cartItems) {
+                const detailedItems = await Promise.all(
+                    cartItems.map(async (item: CartItem) => {
+                        try {
+                            const { result, error } =
+                                await database.getProductVariantMockAfterJoin(
+                                    item.product_id,
+                                    item.variant_id,
+                                    item.image_id,
+                                );
+
+                            if (error) {
+                                console.error("Error fetching product variant data:", error);
+                                return null;
+                            }
+
+                            if (result) {
+                                return {
+                                    ...item,
+                                    title: result.title,
+                                    mock: result.mock,
+                                    size: result.size,
+                                    color: result.color_code,
+                                    price: result.price,
+                                };
+                            }
+                        } catch (err) {
+                            console.error("Error during fetch:", err);
+                            return null;
+                        }
+                    }),
                 );
 
-              if (error) {
-                console.error("Error fetching product variant data:", error);
-                return null;
-              }
-
-              if (result) {
-                return {
-                  ...item, // Basic cart item properties
-                  title: result.title,
-                  mock: result.mock,
-                  size: result.size,
-                  color: result.color_code,
-                };
-              }
-            } catch (err) {
-              console.error("Error during fetch:", err);
-              return null;
+                const validItems = detailedItems.filter((item) => item !== null);
+                setCartItems(validItems as typeof cartItems);
             }
-          }),
+        } catch (err) {
+            console.error("Unexpected error fetching cart data:", err);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if (user) {
+            fetchCartData();
+        }
+    }, [user]);
+
+    const handleIncrement = async (
+        productId: number,
+        variantId: number,
+        image_id: number,
+    ) => {
+        setCartItems((prevItems) =>
+            prevItems.map((item) =>
+                item.product_id === productId && item.variant_id === variantId
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item,
+            ),
         );
 
-        // Filter out null values from failed fetches
-        const validItems = detailedItems.filter((item) => item !== null);
-        setCartItems(validItems as typeof cartItems); // Update cart state
-      }
-    } catch (err) {
-      console.error("Unexpected error fetching cart data:", err);
-    } finally {
-      setLoading(false);
+        try {
+            const { error } = await incrementCartItem(
+                user!.id,
+                productId,
+                variantId,
+                image_id,
+                database.client,
+            );
+            if (error) {
+                throw new Error(error);
+            }
+        } catch (error) {
+            console.error("Error incrementing item:", error);
+            await fetchCartData();
+        }
+    };
+
+    const handleDecrement = async (productId: number, variantId: number) => {
+        setCartItems((prevItems) =>
+            prevItems.map((item) =>
+                item.product_id === productId && item.variant_id === variantId
+                    ? { ...item, quantity: Math.max(item.quantity - 1, 0) }
+                    : item,
+            ),
+        );
+
+        try {
+            const { error } = await decrementCartItem(
+                user!.id,
+                productId,
+                variantId,
+                database.client,
+            );
+
+            if (error) {
+                throw new Error(error);
+            }
+        } catch (error) {
+            console.error("Error decrementing item:", error);
+            await fetchCartData();
+        }
+    };
+
+    async function removeItemFromCart(productId: number, variantId: number) {
+        setCartItems((prevItems) =>
+            prevItems.filter(
+                (item) =>
+                    !(item.product_id === productId && item.variant_id === variantId),
+            ),
+        );
+
+        try {
+            const { error } = await database.removeCartItem(
+                user!.id,
+                productId,
+                variantId,
+            );
+
+            if (error) {
+                throw new Error(error);
+            }
+        } catch (error) {
+            console.error("Error removing item:", error);
+            await fetchCartData();
+        }
     }
-  }
 
-  useEffect(() => {
-    if (user) {
-      fetchCartData();
-    }
-  }, [user]);
+    const subtotal = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    const shipping = 5.99;
+    const tax = subtotal * 0.08;
 
-  // Optimistic Increment
-  const handleIncrement = async (
-    productId: number,
-    variantId: number,
-    image_id: number,
-  ) => {
-    setCartItems((prevItems) =>
-      prevItems.map((item) =>
-        item.product_id === productId && item.variant_id === variantId
-          ? { ...item, quantity: item.quantity + 1 }
-          : item,
-      ),
-    );
+    return (
+    <div className="flex p-6 bg-white h-screen">
+        <div className="flex-1 pr-4 max-h-[90vh] o ">
+            <div className="h-full pr-2 ">
+                <h2 className="text-3xl font-bold text-gray-900 mb-6">Your Shopping Cart</h2>
 
-    try {
-      const { error } = await incrementCartItem(
-        user!.id,
-        productId,
-        variantId,
-        image_id,
-        database.client,
-      );
-      if (error) {
-        throw new Error(error);
-      }
-    } catch (error) {
-      console.error("Error incrementing item:", error);
-      await fetchCartData(); // Revert optimistic update on error
-    }
-  };
+                {loading ? (
+                    <p className="text-gray-600">Loading...</p>
+                ) : cartItems.length === 0 ? (
+                    <p className="text-gray-600">Your cart is empty.</p>
+                ) : (
+                    <ul className="space-y-4">
+                        {cartItems.map((item) => (
+                            <li
+                                key={`${item.product_id}-${item.variant_id}`}
+                                className="flex items-center justify-between shadow-lg p-4 rounded-xl border-b hover:shadow-xl transition-all"
+                            >
+                                <div className="flex items-center space-x-4 flex-1">
+                                    {item.mock && (
+                                        <img
+                                            src={item.mock}
+                                            alt={item.title}
+                                            className="w-30 h-24 object-cover rounded-md shadow-md"
+                                        />
+                                    )}
+                                    <div className="w-3/4">
+                                        <h3 className="text-lg font-semibold text-gray-800">{item.title}</h3>
+                                        <p className="text-sm text-gray-500">Product Id: {item.product_id}</p>
+                                        <p className="text-sm text-gray-500">Variant Id: {item.variant_id}</p>
+                                        <p className="text-sm text-gray-500">Size: {item.size}</p>
+                                        <p className="text-sm text-gray-500">Color: {item.color}</p>
+                                        <div className="flex items-center space-x-4 mt-2">
+                                            <span className="text-sm text-gray-700">${item.price}</span>
+                                            <button
+                                                onClick={() => handleDecrement(item.product_id, item.variant_id)}
+                                                className="h-8 w-8 bg-gray-100 hover:bg-gray-200 rounded-full flex items-center justify-center text-lg font-bold"
+                                            >
+                                                <MinusIcon size={16} />
+                                            </button>
+                                            <span className="w-8 text-center font-medium">{item.quantity}</span>
+                                            <button
+                                                onClick={() => handleIncrement(item.product_id, item.variant_id, item.image_id)}
+                                                className="h-8 w-8 bg-gray-100 hover:bg-gray-200 rounded-full flex items-center justify-center text-lg font-bold"
+                                            >
+                                                <PlusIcon size={16} />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <TrashIcon
+                                    onClick={() => removeItemFromCart(item.product_id, item.variant_id)}
+                                    className="w-6 h-6 text-red-500 cursor-pointer hover:text-red-700"
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </div>
 
-  // Optimistic Decrement
-  const handleDecrement = async (productId: number, variantId: number) => {
-    setCartItems((prevItems) =>
-      prevItems.map((item) =>
-        item.product_id === productId && item.variant_id === variantId
-          ? { ...item, quantity: Math.max(item.quantity - 1, 0) }
-          : item,
-      ),
-    );
+        <div className="w-1/4 pl-4 sticky top-6">
+            <div className="bg-white p-6 shadow-md rounded-lg">
+                <h3 className="text-xl font-semibold text-gray-900 mb-4">Price Summary</h3>
+                <div className="space-y-2">
+                    <div className="flex justify-between">
+                        <span className="text-gray-700">Subtotal</span>
+                        <span className="text-gray-700">${subtotal.toFixed(2)}</span>
+                    </div>
+                    <div className="border-t mt-4 pt-4">
+                        <div className="flex justify-between font-semibold text-lg">
+                            <span>Total</span>
+                            <span>${(subtotal + shipping + tax).toFixed(2)}</span>
+                        </div>
+                    </div>
+                </div>
 
-    try {
-      const { error } = await decrementCartItem(
-        user!.id,
-        productId,
-        variantId,
-        database.client,
-      );
-
-      if (error) {
-        throw new Error(error);
-      }
-    } catch (error) {
-      console.error("Error decrementing item:", error);
-      await fetchCartData(); // Revert optimistic update on error
-    }
-  };
-
-  async function removeItemFromCart(productId: number, variantId: number) {
-    // Optimistically update the quantity in the cart
-    setCartItems((prevItems) =>
-      prevItems.filter(
-        (item) =>
-          !(item.product_id === productId && item.variant_id === variantId),
-      ),
-    );
-
-    try {
-      // Sync with database to ensure data consistency
-      const { error } = await database.removeCartItem(
-        user!.id,
-        productId,
-        variantId,
-      );
-
-      if (error) {
-        throw new Error(error);
-      }
-    } catch (error) {
-      console.error("Error decrementing item:", error);
-      // Optionally revert the optimistic update on error
-      await fetchCartData();
-    }
-  }
-
-  return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Your Cart</h2>
-      {loading ? (
-        <p>Loading...</p>
-      ) : cartItems.length === 0 ? (
-        <p>Your cart is empty</p>
-      ) : (
-        <ul className="space-y-4">
-          {JSON.stringify(cartItems)}
-          {cartItems?.map((item) => (
-            <li
-              key={`${item.product_id}-${item.variant_id}`}
-              className="flex items-center justify-between border p-4 rounded-md"
-            >
-              <div>
-                <p>Product ID: {item.product_id}</p>
-                <p>Variant ID: {item.variant_id}</p>
-              </div>
-              <div className="flex items-center space-x-2">
                 <button
-                  onClick={() =>
-                    handleDecrement(item.product_id, item.variant_id)
-                  }
-                  className="h-8 w-8 bg-gray-200 hover:bg-gray-300 rounded"
+                    className="w-full mt-6 py-2 bg-yellow-600 text-white font-bold rounded-md hover:bg-yellow-700"
+                    onClick={() => alert("Proceed to checkout")}
                 >
-                  -
+                    Proceed to Checkout
                 </button>
-                <span className="w-8 text-center">{item.quantity}</span>
-                <button
-                  onClick={() =>
-                    handleIncrement(
-                      item.product_id,
-                      item.variant_id,
-                      item.image_id,
-                    )
-                  }
-                  className="h-8 w-8 bg-gray-200 hover:bg-gray-300 rounded"
-                >
-                  +
-                </button>
-
-                <TrashIcon
-                  onClick={() =>
-                    removeItemFromCart(item.product_id, item.variant_id)
-                  }
-                />
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+            </div>
+        </div>
     </div>
-  );
+);
 }
