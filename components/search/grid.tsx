ae1623aaad49c72dd93667227399cd9673c91849
visualstream <visualstream709@gmail.com>
2024-12-25 16:15:00 +0600

    u

--- components/search/grid.tsx
+++ components/search/grid.tsx
@@ -2,7 +2,7 @@ import { Image } from "@/database/functions/images/getImagesFromDatabase";
 import { FullPageSpinner } from "../spinners/fullPageSpiner";
 import { FullContainerSpinner } from "../spinners/fullContainerSpinner";
 import { useEffect, useRef, useState } from "react";
-import { CircleDashed, XIcon } from "lucide-react";
+import { ChevronLeft, ChevronRight, CircleDashed, XIcon } from "lucide-react";
 import Link from "next/link";
 import { ImageWithSimilarity } from "@/libs/ColorAnalyzer/colorAnalyzer";
 
@@ -51,16 +51,70 @@ function chunkArray<T>(array: T[], chunkSize: number): T[][] {
 }
 
 function BentoGrid({ images }: { images: ImageWithSimilarity[] }) {
-  // Split the images array into chunks of 7 elements
+  let count = 60;
+  let [page, setPage] = useState(1);
+  let PER_PAGE = 51;
 
-  let count = 48;
-  const imageChunks = chunkArray(
-    images,
-    // images.filter((image) => image.similarity > 0.5),
-    count,
-  );
+  let startIndex = (page - 1) * PER_PAGE;
+  let endIndex = page * PER_PAGE;
+  let currentImages = [...images].slice(startIndex, endIndex);
+  let maxPage = Math.ceil(images.length / PER_PAGE);
+  const imageChunks = chunkArray(currentImages, count);
+
+  useEffect(() => {
+    setPage(1);
+  }, [images]);
+
+  function handleNextPage() {
+    if (page < maxPage) {
+      setPage(page + 1);
+    }
+  }
+
+  function handlePreviousPage() {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  }
+
+  const generatePageNumbers = () => {
+    const range: number[] = [];
+    const buffer = 2; // Number of pages to show around the current page
+
+    // Add the first page
+    range.push(1);
+
+    // Add pages around the current page
+    for (
+      let i = Math.max(2, page - buffer);
+      i <= Math.min(maxPage - 1, page + buffer);
+      i++
+    ) {
+      range.push(i);
+    }
+
+    // Add the last page
+    if (maxPage > 1) {
+      range.push(maxPage);
+    }
+
+    // Insert `...` where needed
+    const visiblePages: (number | "...")[] = [];
+    range.forEach((num, index) => {
+      if (index > 0 && num - range[index - 1] > 1) {
+        visiblePages.push("...");
+      }
+      visiblePages.push(num);
+    });
+
+    return visiblePages;
+  };
 
   function getColSpan(i: number) {
+    if (currentImages.length < PER_PAGE) {
+      return i % 2 == 0 ? "col-span-2" : "col-span-1";
+    }
+
     if (i === 1) return "col-span-2";
     if (i === 5) return "col-span-2";
     if (i === 6) return "col-span-2";
@@ -68,10 +122,17 @@ function BentoGrid({ images }: { images: ImageWithSimilarity[] }) {
     if (i === 17) return "col-span-2";
     if (i === 23) return "col-span-2";
     if (i === 28) return "col-span-2";
+    //
+    if (i === 41) return "col-span-2";
+    if (i === 50) return "col-span-2";
     return "";
   }
 
   function getRowSpan(i: number) {
+    if (currentImages.length < PER_PAGE) {
+      return "row-span-1";
+    }
+
     if (i === 0) return "row-span-2";
     if (i === 2) return "row-span-2";
     if (i === 3) return "row-span-3";
@@ -84,6 +145,13 @@ function BentoGrid({ images }: { images: ImageWithSimilarity[] }) {
     if (i === 23) return "row-span-2";
     if (i === 25) return "row-span-2";
     if (i === 28) return "row-span-2";
+    //
+    if (i === 30) return "row-span-2";
+    if (i === 37) return "row-span-2";
+    if (i === 40) return "row-span-2";
+    if (i === 41) return "row-span-2";
+    if (i === 42) return "row-span-2";
+    if (i === 45) return "row-span-2";
     return "";
   }
 
@@ -127,108 +195,149 @@ function BentoGrid({ images }: { images: ImageWithSimilarity[] }) {
   }, [selectedImage]);
 
   return (
-    <div className="w-[calc(100%-80px)] m-auto max-h-[calc(100%-80px)] h-[calc(100%-80px)] overflow-y-auto overflow-x-hidden custom-scrollbar flex flex-col">
-      {imageChunks.map((chunk, chunkIndex) => (
-        <div
-          key={chunkIndex}
-          className="grid auto-rows-[70px] grid-cols-12 gap-1"
-          ref={gridRef}
-        >
-          {chunk.map((image, i) => (
-            <div
-              key={image.id}
-              className={`${getRowSpan(i)} rounded-xl ${getColSpan(i)} relative`}
-            >
-              <div className="w-full h-full relative">
-                {selectedImage?.id === image.id && modalPosition && (
-                  <div
-                    className={`absolute flex flex-col gap-4 top-0 w-[300px] h-[max-content] bg-white z-20 p-4 shadow-md border border-black
+    <>
+      <div
+        className={`w-[calc(100%-80px)] m-auto max-h-[calc(100%-80px)] h-[calc(100%-80px)] overflow-y-auto overflow-x-hidden custom-scrollbar flex flex-col
+          ${currentImages?.length < PER_PAGE ? "pt-[100px]" : "pt-[100px]"}`}
+      >
+        {imageChunks.map((chunk, chunkIndex) => (
+          <div
+            key={chunkIndex}
+            className="grid auto-rows-[70px] grid-cols-12 gap-1"
+            ref={gridRef}
+          >
+            {chunk.map((image, i) => (
+              <div
+                key={image.id}
+                className={`${getRowSpan(i)} rounded-xl ${getColSpan(i)} relative`}
+              >
+                {/* <p className="absolute z-40 font-bold bg-[#00000040] text-white bottom-0 text-center">
+                  {i}
+                </p> */}
+                <div className="w-full h-full relative">
+                  {selectedImage?.id === image.id && modalPosition && (
+                    <div
+                      className={`absolute flex flex-col gap-4 top-0 w-[300px] h-[max-content] bg-white z-20 p-4 shadow-md border border-black
                       transition-transform duration-300
                       ${
                         modalPosition === "left" && selectedImage
                           ? "left-0 -translate-x-[300px]"
                           : "right-0 translate-x-[300px]"
                       }`}
-                  >
-                    <XIcon
-                      className="cursor-pointer ml-auto"
-                      onClick={() => {
-                        setSelectedImage(null);
-                        selectedImageRef.current = null;
-                      }}
-                    />
-                    <img src={image.image_url!} alt={image.ai_describe!} />
-                    <Link href={`/image/${image.id}`}>
-                      <p className="text-blue-500 w-full text-center">
-                        View Products
-                      </p>
-                    </Link>
-                    {false && (
-                      <div
-                        className="w-10 h-10 rounded-full"
-                        style={{
-                          // @ts-ignore
-                          backgroundColor: image.bestMatchColor,
+                    >
+                      <XIcon
+                        className="cursor-pointer ml-auto"
+                        onClick={() => {
+                          setSelectedImage(null);
+                          selectedImageRef.current = null;
                         }}
-                      >
-                        {/* @ts-ignore */}
-                      </div>
-                    )}
-                    {false && <p> {image.similarity}</p>}
-                    {false && image.color_composition && (
-                      <div className="flex flex-col gap-4">
-                        <h1 className="font-bold text-lg">Color Composition</h1>
-                        <div className="flex items-end gap-2">
+                      />
+                      <img src={image.image_url!} alt={image.ai_describe!} />
+                      <Link href={`/image/${image.id}`}>
+                        <p className="text-blue-500 w-full text-center">
+                          View Products
+                        </p>
+                      </Link>
+                      {false && (
+                        <div
+                          className="w-10 h-10 rounded-full"
+                          style={{
+                            // @ts-ignore
+                            backgroundColor: image.bestMatchColor,
+                          }}
+                        >
                           {/* @ts-ignore */}
-                          {image.color_composition.map((palette, index) => (
-                            <div
-                              key={index}
-                              className="flex flex-col items-center"
-                            >
-                              <div
-                                className="w-10"
-                                style={{
-                                  height: `${palette.percentage * 2}px`,
-                                  backgroundColor: palette.color,
-                                  borderRadius: "4px",
-                                }}
-                              />
-                              <p className="text-sm mt-1">
-                                {parseFloat(palette.percentage).toFixed(2)}%
-                              </p>
-                              <div className="w-10 h-10 rounded-full border border-black">
-                                <div
-                                  className="w-full h-full rounded-full"
-                                  style={{ backgroundColor: palette.color }}
-                                />
-                              </div>
-                            </div>
-                          ))}
                         </div>
-                      </div>
-                    )}
-                  </div>
-                )}
+                      )}
+                      {false && <p> {image.similarity}</p>}
+                      {false && image.color_composition && (
+                        <div className="flex flex-col gap-4">
+                          <h1 className="font-bold text-lg">
+                            Color Composition
+                          </h1>
+                          <div className="flex items-end gap-2">
+                            {/* @ts-ignore */}
+                            {image.color_composition.map((palette, index) => (
+                              <div
+                                key={index}
+                                className="flex flex-col items-center"
+                              >
+                                <div
+                                  className="w-10"
+                                  style={{
+                                    height: `${palette.percentage * 2}px`,
+                                    backgroundColor: palette.color,
+                                    borderRadius: "4px",
+                                  }}
+                                />
+                                <p className="text-sm mt-1">
+                                  {parseFloat(palette.percentage).toFixed(2)}%
+                                </p>
+                                <div className="w-10 h-10 rounded-full border border-black">
+                                  <div
+                                    className="w-full h-full rounded-full"
+                                    style={{ backgroundColor: palette.color }}
+                                  />
+                                </div>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  )}
 
-                <img
-                  src={image.low_resolution_image_url!}
-                  alt={image.ai_describe!}
-                  className={`w-full h-full object-cover`}
-                  onClick={() => {
-                    if (selectedImage) {
-                      setSelectedImage(image);
-                    } else {
-                      setSelectedImage(image);
+                  <img
+                    src={image.low_resolution_image_url!}
+                    alt={image.ai_describe!}
+                    className={`w-full h-full object-cover`}
+                    onClick={() => {
+                      if (selectedImage) {
+                        setSelectedImage(image);
+                      } else {
+                        setSelectedImage(image);
+                      }
+                    }}
+                    ref={
+                      selectedImage?.id === image.id ? selectedImageRef : null
                     }
-                  }}
-                  ref={selectedImage?.id === image.id ? selectedImageRef : null}
-                />
+                  />
+                </div>
               </div>
-            </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      {
+        // pagination, show prev and next buttons and the dynamic page number
+        <div className="flex justify-center items-center gap-4 my-4 mt-auto">
+          <button
+            onClick={handlePreviousPage}
+            className="bg-blue-300 p-2 rounded-full"
+          >
+            <ChevronLeft size={24} />
+          </button>
+          {generatePageNumbers().map((current, index) => (
+            <button
+              key={index}
+              onClick={() => typeof current === "number" && setPage(current)}
+              className={`p-2 rounded-md ${
+                page === current ? "underline text-blue-600 font-bold" : ""
+              }`}
+              disabled={current === "..."}
+            >
+              {current}
+            </button>
           ))}
+          <button
+            onClick={handleNextPage}
+            className="bg-blue-300 p-2 rounded-full"
+          >
+            <ChevronRight size={24} />
+          </button>
         </div>
-      ))}
-    </div>
+      }
+    </>
   );
 }
 
