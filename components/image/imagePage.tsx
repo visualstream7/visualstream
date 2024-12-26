e2c380aa281ba9b07cfea8eccc863521fe19d1af
visualstream <visualstream709@gmail.com>
2024-12-26 19:01:20 +0600

    u

--- components/image/imagePage.tsx
+++ components/image/imagePage.tsx
@@ -272,13 +272,15 @@ export default function ImagePage({ user, image }: UserPropType) {
           }
 
           {hoveredImage && (
-            <h2 className="text-2xl text-gray-900 text-center m-4 lg:w-[30vw]">
+            <h2 className="text-2xl text-gray-900 text-center m-4 lg:w-[30vw] h-[10vh]">
               {extract_type_name(getProductFromMock(hoveredImage, products))}
             </h2>
           )}
-          <p className="text-gray-600 text-left m-4 lg:m-0 lg:w-[30vw]">
-            {image.ai_describe || "Description"}
-          </p>
+          {!hoveredImage && (
+            <p className="text-gray-600 text-left m-4 lg:m-0 lg:w-[30vw] lg:h-[15vh] overflow-y-auto no-scrollbar cursor-pointer">
+              {image.ai_describe || "Description"}
+            </p>
+          )}
         </div>
 
         <div className="flex-1 p-6 lg:p-12 space-y-4 bg-white h-[86vh] lg:overflow-y-hidden flex flex-col">
