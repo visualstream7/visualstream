b6e3c4f5eaa88f31878606070dc4276301bd4548
visualstream <visualstream709@gmail.com>
2025-01-29 03:17:19 +0600

    a

--- libs/printful-client/getOrderObject.ts
+++ libs/printful-client/getOrderObject.ts
@@ -1,11 +1,96 @@
+import { Pi } from "lucide-react";
+
+const PRODUCTS = {
+  PAPER_POSTER: 1,
+  CANVAS: 3,
+  MUG: 19, // done
+  T_SHIRT: 71, // done
+  TOTE_BAG: 84, // done
+  PHONE_CASE: 181, // done
+  HAT: 206,
+  BAGPACK: 279,
+  STICKERS: 358,
+  HOODIE: 380, // done
+  WATER_BOTTLE: 382, // done
+  LAPTOP_SLEEVE: 394, // done
+  SPIRAL_NOTEBOOK: 474, // done
+  JIGSAW_PUZZLE: 534, // done
+  METAL_PRINT: 588,
+};
+
 function getOrderObject(item: any, dimensions: any) {
   const aspectRatio = dimensions.width / dimensions.height;
-  const area_width = 750;
-  const area_height = 1000;
-  const image_width = area_width;
-  const image_height = image_width / aspectRatio;
-  const height_left = area_height - image_height;
-  const top = height_left / 2;
+  let area_width = 0;
+  let area_height = 0;
+  let image_width = 0;
+  let image_height = 0;
+  let height_left = 0;
+  let top = 0;
+
+  if (item.product_id === PRODUCTS.T_SHIRT) {
+    area_width = 750;
+    area_height = 1000;
+    image_width = area_width;
+    image_height = image_width / aspectRatio;
+    height_left = area_height - image_height;
+    top = height_left / 2;
+  } else if (item.product_id === PRODUCTS.HOODIE) {
+    area_width = 1000;
+    area_height = 1000;
+    image_width = area_width;
+    image_height = image_width / aspectRatio;
+    height_left = area_height - image_height;
+    top = height_left / 2;
+  } else if (item.product_id === PRODUCTS.MUG) {
+    area_width = 2570;
+    area_height = 1000;
+    image_width = area_width;
+    image_height = image_width / aspectRatio;
+    height_left = area_height - image_height;
+    top = height_left / 2;
+  } else if (item.product_id === PRODUCTS.WATER_BOTTLE) {
+    area_width = 1620;
+    area_height = 1000;
+    image_width = area_width;
+    image_height = image_width / aspectRatio;
+    height_left = area_height - image_height;
+    top = height_left / 2;
+  } else if (item.product_id === PRODUCTS.LAPTOP_SLEEVE) {
+    area_width = 1300;
+    area_height = 1000;
+    image_width = area_width;
+    image_height = image_width / aspectRatio;
+    height_left = area_height - image_height;
+    top = height_left / 2;
+  } else if (item.product_id === PRODUCTS.SPIRAL_NOTEBOOK) {
+    area_width = 660;
+    area_height = 1000;
+    image_width = area_width;
+    image_height = image_width / aspectRatio;
+    height_left = area_height - image_height;
+    top = height_left / 2;
+  } else if (item.product_id === PRODUCTS.JIGSAW_PUZZLE) {
+    area_width = 790;
+    area_height = 1000;
+    image_width = area_width;
+    image_height = image_width / aspectRatio;
+    height_left = area_height - image_height;
+    top = height_left / 2;
+  } else if (item.product_id === PRODUCTS.TOTE_BAG) {
+    area_width = 1030;
+    area_height = 1000;
+    image_width = area_width;
+    image_height = image_width / aspectRatio;
+    height_left = area_height - image_height;
+    top = height_left / 2;
+  } else if (item.product_id === PRODUCTS.PHONE_CASE) {
+    area_width = 480;
+    area_height = 1000;
+    image_width = area_width;
+    image_height = image_width / aspectRatio;
+    height_left = area_height - image_height;
+    top = height_left / 2;
+  }
 
   return {
     variant_id: item.variant_id,
@@ -19,7 +104,7 @@ function getOrderObject(item: any, dimensions: any) {
           area_height: area_height,
           width: image_width,
           height: image_height,
-          top: top,
+          top: top < 0 ? 0 : top,
           left: 0,
         },
       },
--- pages/api/order-test.ts
+++ pages/api/order-test.ts
@@ -1,6 +1,5 @@
 import { SupabaseWrapper } from "@/database/supabase";
 import { getOrderObject } from "@/libs/printful-client/getOrderObject";
-import { clerkClient, currentUser, getAuth } from "@clerk/nextjs/server";
 import { loadImage } from "canvas";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
@@ -9,6 +8,40 @@ export interface ImageDimensions {
   width: number;
   height: number;
 }
+
+let NEXT_PUBLIC_PRINTFUL_TOKEN = "VEY7LvhvkCjjOtpTEtBIbaEadcdDLmaRYvLczI56";
+
+const PRODUCTS = {
+  PAPER_POSTER: 1,
+  CANVAS: 3,
+  MUG: 19,
+  T_SHIRT: 71,
+  TOTE_BAG: 84,
+  PHONE_CASE: 181,
+  HAT: 206,
+  BAGPACK: 279,
+  STICKERS: 358,
+  HOODIE: 380,
+  WATER_BOTTLE: 382,
+  LAPTOP_SLEEVE: 394,
+  SPIRAL_NOTEBOOK: 474,
+  JIGSAW_PUZZLE: 534,
+  METAL_PRINT: 588,
+};
+
+const PRODUCT_VARIANTS = {
+  MUG_VARIANT: 1320,
+  T_SHIRT_VARIANT: 4011,
+  TOTE_BAG_VARIANT: 4533,
+  PHONE_CASE_VARIANT: 7910,
+  HAT_VARIANT: 7853,
+  BAGPACK_VARIANT: 9063,
+  HOODIE_VARIANT: 10774,
+  WATER_BOTTLE_VARIANT: 10798,
+  LAPTOP_SLEEVE_VARIANT: 10985,
+  SPIRAL_NOTEBOOK_VARIANT: 12141,
+  JIGSAW_PUZZLE_VARIANT: 13431,
+};
 
 async function getImageDimensions(url: string): Promise<ImageDimensions> {
   console.log("url", url);
@@ -28,16 +61,51 @@ export default async function handler(
 
   const IMAGE_URL =
     "https://utfs.io/f/8t9iMYLAkULvtfVP55b9HMxIfSmQ2w4nscqr9lTK0huJyCNY";
+  // const IMAGE_URL =
+  //   "https://utfs.io/f/8t9iMYLAkULvZ7Y7MqNjrOEcbwCa8Jm3xhUXqpko9QvRIu0i";
   const { height, width } = await getImageDimensions(IMAGE_URL);
-
   const order = getOrderObject(
     {
       image: IMAGE_URL,
-      variant_id: 1,
+      product_id: PRODUCTS.PHONE_CASE,
+      variant_id: PRODUCT_VARIANTS.PHONE_CASE_VARIANT,
       quantity: 1,
     },
     { height, width },
   );
 
-  res.status(200).json({ result: { height, width }, error: null });
+  const data = JSON.stringify({
+    store_id: 14818720,
+    recipient: {
+      name: "maruf",
+      address1: "123 Main St",
+      city: "Los Angeles",
+      state_code: "CA",
+      country_code: "US",
+      zip: "90007",
+    },
+    items: [{ ...order }],
+  });
+
+  try {
+    const response = await fetch("https://api.printful.com/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${NEXT_PUBLIC_PRINTFUL_TOKEN}`,
+      },
+      body: data,
+    });
+
+    let rData = await response.json();
+    return res.status(200).json({
+      dimensions: { height, width },
+      result: rData,
+      order: order,
+    });
+  } catch (error) {
+    console.error("Error during fetch:", error);
+  }
+
+  return res.status(200).json({ result: null, error: "Error during fetch" });
 }
