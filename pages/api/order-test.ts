8012f57a8bf780cb130165d9b6a5c6288153c34f
visualstream <visualstream709@gmail.com>
2025-01-29 01:02:40 +0600

    a

--- pages/api/order-test.ts
+++ pages/api/order-test.ts
@@ -1,4 +1,5 @@
 import { SupabaseWrapper } from "@/database/supabase";
+import { getOrderObject } from "@/libs/printful-client/getOrderObject";
 import { clerkClient, currentUser, getAuth } from "@clerk/nextjs/server";
 import { loadImage } from "canvas";
 import type { NextApiRequest, NextApiResponse } from "next";
@@ -25,8 +26,18 @@ export default async function handler(
     return res.status(405).json({ result: null, error: "Method not allowed" });
   }
 
-  const IMAGE_URL = "";
+  const IMAGE_URL =
+    "https://utfs.io/f/8t9iMYLAkULvtfVP55b9HMxIfSmQ2w4nscqr9lTK0huJyCNY";
   const { height, width } = await getImageDimensions(IMAGE_URL);
+
+  const order = getOrderObject(
+    {
+      image: IMAGE_URL,
+      variant_id: 1,
+      quantity: 1,
+    },
+    { height, width },
+  );
 
   res.status(200).json({ result: { height, width }, error: null });
 }
