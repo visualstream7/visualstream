ae16e8a9e55b3706de36132c7e8403526b5d97b5
visualstream <visualstream709@gmail.com>
2025-02-07 00:54:57 +0600

    a

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { appearance } from "@/data/clerk_appearance";
 import { ToastContainer } from "react-toastify";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider appearance={appearance}>
+      <GoogleAnalytics gaMeasurementId="G-E299JNFPKD" />
       <Component {...pageProps} />
       <ToastContainer />
     </ClerkProvider>
