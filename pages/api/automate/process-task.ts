62ca57f8a1bcb3b42aa28d74edad687f94f62387
visualstream <visualstream709@gmail.com>
2025-03-20 00:26:43 +0600

    changes

--- pages/api/automate/process-task.ts
+++ pages/api/automate/process-task.ts
@@ -1,11 +1,24 @@
 import { SupabaseWrapper } from "@/database/supabase";
 import { NextApiRequest, NextApiResponse } from "next";
 
+// This function can run for a maximum of 5 seconds
+export const config = {
+  maxDuration: 800,
+};
+
+async function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   if (req.method !== "GET") {
+    // wait for 40 seconds
+
+    await sleep(40000);
+
     return res.status(405).json({ result: null, error: "Method Not Allowed" });
   }
 
