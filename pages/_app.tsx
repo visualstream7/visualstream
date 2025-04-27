ea1ef87d13486496e1d01651c5e52719e817c574
visualstream <visualstream709@gmail.com>
2025-04-27 13:10:01 +0600

    u

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,28 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { appearance } from "@/data/clerk_appearance";
 import { ToastContainer } from "react-toastify";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import { useState } from "react";
+
+function LaunchingSoon({ password, setPassword }: any) {
+  return (
+    <div>
+      <h1>Launching Soon</h1>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Enter password"
+      />
+    </div>
+  );
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [password, setPassword] = useState("");
+
+  if (password !== "12345678") {
+    return <LaunchingSoon password={password} setPassword={setPassword} />;
+  }
   return (
     <ClerkProvider appearance={appearance}>
       <GoogleAnalytics gaMeasurementId="G-E299JNFPKD" />
